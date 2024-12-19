47da73aea8b31b488111cfbd9e2ed73a52e7a26b
Majd <majd.osman@elevoro.com>
2024-12-19 13:12:19 +0200

    fix: update pagination logic to accommodate zero based indexing

--- src/api/articles.tsx
+++ src/api/articles.tsx
@@ -1,16 +1,16 @@
-import { axiosClient } from "./axios_client";
+import axios from "axios";
 
 export const getPaginatedArticles = async (limit: number, page: number) => {
-    return axiosClient.get("/topstories.json").then((res) => {
+    return axios.get("https://hacker-news.firebaseio.com/v0/topstories.json").then((res) => {
         return {
             totalPages: Math.ceil(res.data.length / limit),
-            list: res.data.slice((page - 1) * limit, (page + 1) * limit)
+            list: res.data.slice((page - 1) * limit, Math.min(((page - 1) * limit) + limit, res.data.length))
         }
     });
 };
 
 export const getArticle = async (id: number) => {
-    return axiosClient.get(`/item/${id}.json`).then((res) => {
+    return axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then((res) => {
         return res.data
     });
 };
